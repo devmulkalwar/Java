045e83dc09400c8c224b3de089ef8d348fd6e787
devmulkalwar <devmulkalwar95@gmail.com>
2024-11-09 20:34:26 +0530

    anagram program added

---
+++ JavaBasics/Anagram.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first string :");
+        String str1 = scanner.next().toLowerCase();;
+
+        System.out.println("Enter second string :");
+        String str2 = scanner.next().toLowerCase();
+
+        if(str1.length() == str2.length()){
+            String result1 = sortAlphabetically( str1);
+            String result2 = sortAlphabetically( str2);
+            if (result1.equals(result2)){
+                System.out.println("Strings are anangram");
+            }
+            else {
+                System.out.println("Strings are not anagram");
+            }
+        }
+        else {
+            System.out.println("Strings are not anagram");
+        }
+        scanner.close();
+    }
+
+    public static String sortAlphabetically(String str) {
+
+        char[] charArray = str.toCharArray();
+
+        Arrays.sort(charArray);
+
+        return new String(charArray);
+    }
+}
