59bcd87d838bebfe1d09c771ca96d51bb7f70487
devmulkalwar <devmulkalwar95@gmail.com>
2024-11-15 17:35:43 +0530

    minor corrections in string pallindrome code

--- JavaBasics/PalindromeString.java
+++ JavaBasics/PalindromeString.java
@@ -1,23 +1,22 @@
 import java.util.*;
-
-public class PalindromeStr {
+public class PalindromeString {
     public static void main(String[] args) {
-      
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word:");
         String str = sc.next().toLowerCase();
         String reverseStr = "";
-      
+
         for (int i = str.length() - 1; i >= 0; i--) {
             reverseStr += str.charAt(i);
         }
-      
+
         if (str.equalsIgnoreCase(reverseStr)) {
             System.out.println(str + " is a palindrome.");
         } else {
             System.out.println(str + " is not a palindrome.");
         }
-      
+
         sc.close();
     }
 }
